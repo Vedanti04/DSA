1d1cd91e455890a568275a3fd51911a0f9a03750
Vedanti Deshmukh <vedantideshmukh327@gmail.com>
2026-02-01 11:40:28 +0530

    Day 7: Vector done

---
+++ Day-06_STL/Vector.cpp
@@ -0,0 +1,42 @@
+//Vector : A dynamic array (size can grow/shrink automatically).
+// It stores values in similar pattern as array does .
+
+void explainVector(){
+
+    vector<int> v;     // empty
+
+    v.push_back(1);     // add to end 
+    v.emplace_back(2);  // slightly faster than push_back
+
+    vector<pair<int, int>> vec;    //Vector  of pairs 
+
+    v.push_back({1,2});
+    v.emplace_back(1,2);
+
+    vector<int> v(5,100);   //{100,100,100,100,100} declares  5 instances of 100
+
+    vector<int> v(5);       //{0,0,0,0,0} garbage value instances inserted 
+
+    vector<int> v1(5,20);    // {20,20,20,20,20} declares  5 instances of 20
+    vector<int> v2(v1);      //copy of v1 in another container declares  5 instances of 20
+    
+    // you can always increse the size of vector even if you are predefining the size 
+
+    vector <int>::iterator it = v.begin();       //v.begin() = points at the starting memory 
+    
+    it++;                                       // shifts on side and points at the memory 
+    cout << *(it) << " ";     // *(it)= acesses the value of that memory 
+
+    it = it + 2;
+    cout << *(it) << " ";      
+
+     vector <int>::iterator it = v.end();  
+     vector <int>::iterator it = v.rend();     // reverse end 
+     vector <int>::iterator it = v.rbegin();    // reverse begin
+
+
+    cout << v[0] << " " << v.at[0];
+    cout << v.back() <<" ";
+      
+
+}
